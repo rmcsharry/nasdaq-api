07b0798bea89d0b648d92f7b33b6fa98c4a93c9c
Richard McSharry <rmcsharry@gmail.com>
2023-11-07 15:18:01 +0100

    setup postgres

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from typing import Union
-
+import uvicorn
 from fastapi import FastAPI
+from database import Base, engine
 
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
+
+if __name__ == '__main__':
+    uvicorn.run('app:app', host='0.0.0.0', port=8080, reload=True)
 
 @app.get("/")
 def read_root():
--- database.py
+++ database.py
@@ -1,3 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
+from config import get_settings
+
+settings = get_settings()
+
+DATABASE_URL = f'postgresql://{settings.DB_USERNAME}:{settings.DB_PASSWORD}@localhost:6000/{settings.DB_NAME}'
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
