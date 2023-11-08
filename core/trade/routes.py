c8b92d0d03dfebc0eb2849f1ef1162a4c8817b2b
Richard McSharry <rmcsharry@gmail.com>
2023-11-08 14:36:49 +0100

    return streaming json data

--- core/trade/routes.py
+++ core/trade/routes.py
@@ -1,3 +1,5 @@
+import json
+from re import split
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
@@ -8,11 +10,23 @@ from core.trade.models import Trade
 trade_router = APIRouter()
 
 @trade_router.get('/trades')
-async def read_item(symbol: str, db: Session = Depends(get_db)):
+def read_item(symbol: str, db: Session = Depends(get_db)):
     # This won't work, since we don't have an id column in our table
     # q = db.query(Trade).filter_by(symbol=symbol.upper()).limit(100).all()
     # return q
 
     with db as session:
-        trades = session.execute("SELECT symbol, price, cancelled_indicator FROM trades WHERE symbol = :symbol LIMIT 1000", {"symbol": symbol.upper()}).scalars()
-    return StreamingResponse(trades, media_type="application/json")
+        trades = session.execute("SELECT trade_date, symbol, price, cancelled_indicator FROM trades WHERE symbol = :symbol LIMIT 1000", {"symbol": symbol.upper()}).all()
+
+    def iter_trades():
+        for trade in trades:
+            yield json.dumps({
+                "trade_date": str(trade.trade_date),
+                "symbol": trade.symbol,
+                "price": str(trade.price),
+                "cancelled_indicator": trade.cancelled_indicator
+            }) + "\n"
+            # send as csv
+            # yield f"{trade.symbol}, {trade.price}, {trade.cancelled_indicator}\n"
+
+    return StreamingResponse(iter_trades(), media_type="application/json")
